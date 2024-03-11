cb2be2febde2d78c7674715db0c62299f85a54ea
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-11 08:14:02 +0100

    first

--- utils/jwt.js
+++ utils/jwt.js
@@ -20,6 +20,7 @@ const attachCookiesToResponse = ({ res, user }) => {
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
+    sameSite: 'Lax',
   });
 };
 
