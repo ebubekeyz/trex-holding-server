8908565eaf07a1e8986f0e8e74b160c9fee01c6b
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-13 18:34:36 +0100

    eighteenth

--- utils/jwt.js
+++ utils/jwt.js
@@ -15,12 +15,21 @@ const attachCookiesToResponse = ({ res, user }) => {
 
   const oneDay = 1000 * 60 * 60 * 24;
 
+  //   res.cookie('token', token, {
+  //     httpOnly: true,
+  //     expires: new Date(Date.now() + oneDay),
+  //     secure: process.env.NODE_ENV === 'production',
+  //     signed: true,
+  //     sameSite: 'Lax',
+  //   });
+  // };
+
   res.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite: 'Lax',
+    sameSite: 'None',
   });
 };
 
